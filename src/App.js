1ee6a7fe91880ade734d4d48d172e933c8fe2930
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2022-03-15 21:37:24 -0400

    Imported bootstrap into my react app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">My Favorite Dictionary</header>
+      <header className="text-center">My Favorite Dictionary</header>
     </div>
   );
 }

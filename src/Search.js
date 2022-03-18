627f959271e9f60343abb4e281b704694e51a148
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2022-03-18 18:17:19 -0400

    Integrated an api url into the search component to get actual info for searched words

--- src/Search.js
+++ src/Search.js
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 import "./Search.css";
 
 function Search() {
   const [keyword, setKeyword] = useState(null);
+  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+  axios.get(apiUrl).then(handleResponse);
+
+  function handleResponse(response) {
+    console.log(response.data);
+  }
 
   function searchWord(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
   }
 
   function handleSearch(event) {

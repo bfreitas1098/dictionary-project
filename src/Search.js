531c476c34fe40cc24d308f8c19eaa3368b2cbb0
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2022-03-21 20:30:54 -0400

    Created a loaded state and moved the search arguments into its own function

--- src/Search.js
+++ src/Search.js
@@ -7,35 +7,50 @@ import "./Search.css";
 function Search() {
   const [keyword, setKeyword] = useState("");
   const [results, setResults] = useState("");
+  const [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
-  function searchWord(event) {
-    event.preventDefault();
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
   }
 
   function handleSearch(event) {
     setKeyword(event.target.value);
   }
 
-  return (
-    <div className="Search">
-      <form onSubmit={searchWord}>
-        <input
-          type="search"
-          autoComplete="off"
-          autoFocus="off"
-          onChange={handleSearch}
-        />
-        <input type="submit" value="Define" />
-      </form>
-      <Results results={results} />
-    </div>
-  );
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Search">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            autoComplete="off"
+            autoFocus="off"
+            onChange={handleSearch}
+          />
+          <input type="submit" value="Define" />
+        </form>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading...";
+  }
 }
 
 export default Search;

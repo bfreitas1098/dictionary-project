0b4ab09352ac5bbf907f1d46f66ea971807d2c66
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2022-03-23 15:19:46 -0400

    Began the styling for the synonyms component

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -3,9 +3,10 @@ import React from "react";
 import "./Synonyms.css";
 
 function Synonyms(props) {
-  if (props.synonyms) {
+  if (props.synonyms.length > 0) {
     return (
-      <div className="Synonyms text-center">
+      <div className="Synonyms">
+        <h5 className="heading">Synonyms:</h5>
         {props.synonyms.map(function (synonym, index) {
           return <div key={index}>{synonym}</div>;
         })}

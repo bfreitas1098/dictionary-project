7aaf3f4816adc4a373cef721db5c57e365771c15
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2022-03-30 23:33:41 -0400

    Changed the position and font size of the contact line

--- src/Images.js
+++ src/Images.js
@@ -5,23 +5,44 @@ import "./Images.css";
 function Images(props) {
   if (props.images) {
     return (
-      <section className="Images">
-        <div className="row">
-          {props.images.map(function (image, index) {
-            return (
-              <div className="col-4" key={index}>
-                <a href={image.src.original} rel="noreferrer" target="_blank">
-                  <img
-                    src={image.src.landscape}
-                    alt={image.alt}
-                    className="img-fluid"
-                  />
-                </a>
-              </div>
-            );
-          })}
+      <div className="Images">
+        <section>
+          <div className="row">
+            {props.images.map(function (image, index) {
+              return (
+                <div className="col-4" key={index}>
+                  <a href={image.src.original} rel="noreferrer" target="_blank">
+                    <img
+                      src={image.src.landscape}
+                      alt={image.alt}
+                      className="img-fluid"
+                    />
+                  </a>
+                </div>
+              );
+            })}
+          </div>
+        </section>
+        <div className="contact-line">
+          This website was coded by{" "}
+          <a
+            href="https://hopeful-franklin-29cc80.netlify.app/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Barbara Freitas
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/bfreitas1098/dictionary-project"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            open-sourced
+          </a>{" "}
+          on Github.
         </div>
-      </section>
+      </div>
     );
   } else {
     return null;

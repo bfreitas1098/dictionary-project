d53943533061ee09370f594ad6a6034c834ec52b
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2022-04-30 23:05:50 -0400

    Added a side navbar and styled it

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 import React from "react";
+import styled from "styled-components";
 import { Search } from "./Search";
 
+import { theme } from "../styles/ColorStyles";
 import "bootstrap/dist/css/bootstrap.css";
 import "../styles/App.css";
 
 export const App = () => (
   <div className="App">
+    <NavBar>
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid" />
+      </nav>
+    </NavBar>
     <Search />
   </div>
 );
+
+export const NavBar = styled.nav`
+  .navbar {
+    background-color: ${theme.navColor} !important;
+    position: absolute;
+    width: 70px;
+    height: 1000px;
+    left: 0px;
+  }
+`;
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const sectionStyle = styled.section`
   padding: 30px;
-  border-radius: 4px;
+  border-radius: 20px;
   box-shadow: 0 10px 10px rgb(0 0 0 / 10%);
   margin: 20px 0;
 `;

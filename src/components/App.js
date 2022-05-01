f3a7ae839d4689430cca95279495452fecf93986
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2022-05-01 19:58:15 -0400

    Imported an open book icon and added a title to the navbar

--- src/components/App.js
+++ src/components/App.js
@@ -7,16 +7,29 @@ import "bootstrap/dist/css/bootstrap.css";
 import "../styles/App.css";
 
 export const App = () => (
-  <div className="App">
+  <Wrapper>
     <NavBar>
-      <BookIcon></BookIcon>
+      <BookIcon>
+        <i className="fa-solid fa-book-open"></i>
+      </BookIcon>
       <nav className="navbar navbar-light bg-light">
         <div className="container-fluid" />
+        <Title>
+          <span>
+            My Favorite
+            <br />
+            Dictionary.
+          </span>
+        </Title>
       </nav>
     </NavBar>
     <Search />
-  </div>
+  </Wrapper>
 );
+
+export const Wrapper = styled.div`
+  position: relative;
+`;
 
 export const NavBar = styled.nav`
   .navbar {
@@ -24,8 +37,27 @@ export const NavBar = styled.nav`
     position: absolute;
     width: 70px;
     height: 1000px;
-    left: 0px;
+    left: -168px;
   }
 `;
 
-export const BookIcon = styled.i``;
+export const BookIcon = styled.i`
+  position: absolute;
+  left: -150px;
+  top: 55px;
+  z-index: 1;
+  font-size: 33px;
+  color: ${theme.sectionColor};
+`;
+
+export const Title = styled.span`
+  transform: rotate(-90deg);
+  position: absolute;
+  z-index: 1;
+  top: 160px;
+  left: -32px;
+  color: white;
+  font-size: 22px;
+  margin-right: -200px;
+  line-height: 30px;
+`;
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -4,5 +4,4 @@ export const sectionStyle = styled.section`
   padding: 30px;
   border-radius: 20px;
   box-shadow: 0 10px 10px rgb(0 0 0 / 10%);
-  margin: 20px 0;
 `;

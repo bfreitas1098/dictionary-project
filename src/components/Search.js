f65e8aa44733f9022327242d32ebf3653750dfae
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2022-05-01 22:55:44 -0400

    Deleted the CSS for the Search component and restyled the input area

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import axios from "axios";
 import { Results } from "./Results";
 import { Images } from "./Images";
 
-import "../styles/Search.css";
+import { theme } from "../styles/ColorStyles";
 
 export const Search = () => {
   const [keyword, setKeyword] = useState("");
@@ -44,30 +45,61 @@ export const Search = () => {
   };
 
   return (
-    <div className="Search">
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-sm">
-            <form onSubmit={handleSubmit}>
-              <input
-                type="search"
-                autoComplete="off"
-                autoFocus="off"
-                onChange={handleSearch}
-                className="search-box"
-                placeholder="Search a word"
-              />
-              <input type="submit" value="Define" className="search-button" />
-            </form>
-            <div className="images-section">
-              <Images images={images} />
-            </div>
-          </div>
-          <div className="col-sm results-content">
-            <Results results={results} />
-          </div>
-        </div>
-      </div>
-    </div>
+    <Wrapper>
+      <SearchBox>
+        <form>
+          <input
+            type="search"
+            autoComplete="off"
+            autoFocus="off"
+            onChange={handleSearch}
+            placeholder="Search a word"
+          />
+          <button onClick={handleSubmit}>
+            <i className="fa-solid fa-magnifying-glass"></i>
+          </button>
+        </form>
+      </SearchBox>
+      <ImagesWrapper>
+        <Images images={images} />
+      </ImagesWrapper>
+      <ResultsWrapper>
+        <Results results={results} />
+      </ResultsWrapper>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+`;
+
+const SearchBox = styled.div`
+  input {
+    position: absolute;
+    top: 50px;
+    left: 40px;
+    border: solid 1px ${theme.borderColor};
+    border-radius: 20px;
+    padding: 8px;
+    width: 400px;
+    font-size: 16px;
+  }
+
+  i {
+    position: absolute;
+    z-index: 1;
+    color: #dd5e72;
+    top: 60px;
+    left: 400px;
+  }
+
+  button {
+    border: none;
+    background-color: transparent;
+  }
+`;
+
+const ImagesWrapper = styled.div``;
+
+const ResultsWrapper = styled.div``;
